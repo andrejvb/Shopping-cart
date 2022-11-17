9de57720b839117624a6d9621a0c51c86fb53ed0
andrejvb <andre.jvb@gmail.com>
2022-11-17 16:49:38 -0300

    requisito 4

--- src/main.js
+++ src/main.js
@@ -7,7 +7,19 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const sectionProducts = document.querySelector('.products');
 
+const loading = () => {
+  const loadingDiv = document.createElement('div');
+  loadingDiv.className = 'loading';
+  loadingDiv.innerHTML = 'carregando...';
+  return loadingDiv;
+};
+const inicialLoading = loading();
+
+sectionProducts.appendChild(inicialLoading);
+
 const productsList = await fetchProductsList('computador');
+
+sectionProducts.removeChild(inicialLoading);
 
 productsList.forEach((product) => {
   const productElement = createProductElement(product);
